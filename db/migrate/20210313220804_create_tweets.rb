a499106b6bd5e53ed8b9b58d0de223382ea2940b
Allegra✨ <amhilburger@gmail.com>
2021-03-16 19:01:10 -0600

    changed syntax in migration and schema file to list profile as a string

--- db/migrate/20210313220804_create_tweets.rb
+++ db/migrate/20210313220804_create_tweets.rb
@@ -1,6 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[6.1]
   def change
     create_table :tweets do |t|
+      t.string  :profile
       t.string   :title
 	    t.text     :content
 	    t.string   :author
